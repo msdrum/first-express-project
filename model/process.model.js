c6a7397b2777191f47a110eecca45014890e71aa
Marcio Silva <marciosilvadrum@gmail.com>
2022-11-29 16:27:43 -0300

    timestamps added

--- model/process.model.js
+++ model/process.model.js
@@ -1,43 +1,50 @@
 import { Schema, model } from "mongoose";
 
-const processSchema = new Schema({
-  documentName: {
-    type: String,
-    required: true,
-    trim: true,
-    minLength: 2,
-    lowercase: true,
-  },
-  status: {
-    type: String,
-    required: true,
-    minLength: 2,
-    maxLength: 10,
-    trim: true,
-    lowercase: true,
-  },
-  details: {
-    type: String,
-    minLength: 2,
-    lowercase: true,
-  },
-  dateInit: {
-    type: Date,
-  },
-  comments: [
-    {
+const processSchema = new Schema(
+  {
+    documentName: {
       type: String,
+      required: true,
+      trim: true,
+      minLength: 2,
+      lowercase: true,
     },
-  ],
-  dateEnd: {
-    type: Date,
+    status: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 10,
+      trim: true,
+      lowercase: true,
+    },
+    details: {
+      type: String,
+      minLength: 2,
+      lowercase: true,
+    },
+    dateInit: {
+      type: Date,
+      required: true,
+    },
+    comments: [
+      {
+        type: String,
+      },
+    ],
+    dateEnd: {
+      type: Date,
+    },
+    setor: {
+      type: String,
+      required: true,
+      minLength: 2,
+      lowercase: true,
+    },
   },
-  setor: {
-    type: String,
-    minLength: 2,
-    lowercase: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const ProcessModel = model("Process", processSchema);
 
